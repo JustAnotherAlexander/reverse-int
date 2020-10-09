4310205a6007a0e0113db56ebafd982ee4acf6ba
Aliaksandr Matveyankou <aleksandreliuz@gmail.com>
2020-10-09 11:56:15 +0300

    feat: add function that reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  console.log(n);
+  if (n<0) {
+      n=n*-1;
+  }
+  n=n+'';
+  n=n.split('');
+  n.reverse();
+
+  n=n.join('');
+  console.log(n);
+  return n;
 }
